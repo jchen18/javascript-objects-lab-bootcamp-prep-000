7103cfe962caf9a19810514da1bff09f839cc818
Jessie Chen <jessiechen0513@gmail.com>
2018-01-12 03:41:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object,key){
   newObj = Object.assign({},object)
   delete newObj.key
-  return object
+  return newObj
 }
 
 
