9d25ad583614f678907c36942db6253ab09e92b9
Jessie Chen <jessiechen0513@gmail.com>
2018-01-12 03:25:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {cookies: 'heyo'}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  object.assign({},object)[key]=value
+  object.assign({},object,{key:value})[key]=value
   return object
 }
 
